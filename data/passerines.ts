e011d4b29af7473ca12c57d8f0dff9884a0170b5
Motiche <mot.hakimi.95@gmail.com>
2026-02-09 02:14:00 +0330

    neated up birds list a bit

--- data/passerines.ts
+++ data/passerines.ts
@@ -1,15 +1,21 @@
 import { Species } from '../types';
 
 export const PASSERINES: Species[] = [
-    { id: '1', name: 'Barn Swallow', abbreviation: 'BARSWA', family: 'Hirundinidae' },
-    { id: '2', name: 'House Sparrow', abbreviation: 'HOUSPA', family: 'Passeridae' },
-    { id: '3', name: 'Common Blackbird', abbreviation: 'COMBLA', family: 'Turdidae' },
-    { id: '4', name: 'European Robin', abbreviation: 'EURROB', family: 'Muscicapidae' },
-    { id: '5', name: 'Blue Tit', abbreviation: 'BLUTIT', family: 'Paridae' },
-    { id: '6', name: 'Great Tit', abbreviation: 'GRETIT', family: 'Paridae' },
-    { id: '7', name: 'Common Chaffinch', abbreviation: 'COMCHA', family: 'Fringillidae' },
-    { id: '8', name: 'Eurasian Magpie', abbreviation: 'EURMAG', family: 'Corvidae' },
-    { id: '9', name: 'Carrion Crow', abbreviation: 'CARCRO', family: 'Corvidae' },
-    { id: '11', name: 'Common Starling', abbreviation: 'COMSTA', family: 'Sturnidae' },
-    { id: '12', name: 'White Wagtail', abbreviation: 'WHIWAG', family: 'Motacillidae' },
+    { id: '2221', name: 'Barn Swallow', abbreviation: 'BARSWA', family: 'Hirundinidae' },
+    { id: '2222', name: 'House Sparrow', abbreviation: 'HOUSPA', family: 'Passeridae' },
+    { id: '2223', name: 'Common Blackbird', abbreviation: 'COMBLA', family: 'Turdidae' },
+    { id: '2224', name: 'European Robin', abbreviation: 'EURROB', family: 'Muscicapidae' },
+    { id: '2225', name: 'Blue Tit', abbreviation: 'BLUTIT', family: 'Paridae' },
+    { id: '2226', name: 'Great Tit', abbreviation: 'GRETIT', family: 'Paridae' },
+    { id: '2227', name: 'Common Chaffinch', abbreviation: 'COMCHA', family: 'Fringillidae' },
+    { id: '2228', name: 'Eurasian Magpie', abbreviation: 'EURMAG', family: 'Corvidae' },
+    { id: '2229', name: 'Carrion Crow', abbreviation: 'CARCRO', family: 'Corvidae' },
+    { id: '2211', name: 'Common Starling', abbreviation: 'COMSTA', family: 'Sturnidae' },
+  // --- Motacillidae (Wagtails) ---
+    { id: '192', name: 'White Wagtail', abbreviation: 'WHIWAG', family: 'Motacillidae' },
+    { id: '193', name: 'Citrine Wagtail', abbreviation: 'CITWAG', family: 'Motacillidae' },
+    { id: '194', name: 'Water Pipit', abbreviation: 'WATPIP', family: 'Motacillidae' },
+
+  // --- Emberizidae (Buntings) ---
+    { id: '195', name: 'Common Reed Bunting', abbreviation: 'COMBUN', family: 'Emberizidae' }
 ];
--- speciesData.ts
+++ speciesData.ts
@@ -8,14 +8,6 @@ const OTHER: Species[] = [
     { id: '189', name: 'Common Kingfisher', abbreviation: 'COMKIN', family: 'Alcedinidae' },
     { id: '190', name: 'White-throated Kingfisher', abbreviation: 'WHIKIN', family: 'Alcedinidae' },
     { id: '191', name: 'Pied Kingfisher', abbreviation: 'PIEKIN', family: 'Alcedinidae' },
- 
-  // --- Motacillidae (Wagtails) ---
-    { id: '192', name: 'White Wagtail', abbreviation: 'WHIWAG', family: 'Motacillidae' },
-    { id: '193', name: 'Citrine Wagtail', abbreviation: 'CITWAG', family: 'Motacillidae' },
-    { id: '194', name: 'Water Pipit', abbreviation: 'WATPIP', family: 'Motacillidae' },
-
-  // --- Emberizidae (Buntings) ---
-    { id: '195', name: 'Common Reed Bunting', abbreviation: 'COMBUN', family: 'Emberizidae' }
     { id: '310', name: 'Common Wood Pigeon', abbreviation: 'WOOPIG', family: 'Columbidae' },
     { id: '301', name: 'Common Swift', abbreviation: 'COMSWI', family: 'Apodidae' },
     { id: '302', name: 'European Bee-eater', abbreviation: 'EURBEE', family: 'Meropidae' },
